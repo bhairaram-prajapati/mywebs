7919f538d910c94b9af35e3a792740ce9c64c735
Nikuldeora1195 <nikuldeora1195@gmail.com>
2025-07-12 23:35:30 +0530

    added

---
+++ script.js
@@ -0,0 +1,119 @@
+// Simple form alert
+document.addEventListener('DOMContentLoaded', function() {
+    const bottomForm = document.getElementById('bottomForm');
+    if (bottomForm) {
+        bottomForm.addEventListener('submit', function(e) {
+            e.preventDefault();
+            alert('Thank you for contacting us!');
+            bottomForm.reset();
+        });
+    }
+});
+
+
+
+// Tic Tac Toe Game Script
+let boxes=document.querySelectorAll(".box");
+let resetBtn=document.querySelector("#reset-btn");
+let newGamebtn=document.querySelector("#new-btn");
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+
+let turnO=true;//playerX, playerO;
+
+const winPatterns=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("Box was clicked");
+        if(turnO){
+            box.innerText="O";
+            turnO=false;
+        }else{
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled=true;
+        checkWinner();
+
+    });
+});
+let disableBoxes=()=>{
+    for (let box of boxes){
+        box.disabled=true;
+    }
+};
+
+
+let enableBoxes=()=>{
+    for (let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+        box.classList.remove("winning-box");
+    }
+};
+
+const showWinner=(Winner)=>{
+    msg.innerText=`Congratulation! Winner is: ${Winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+
+    confetti({
+    particleCount: 150,
+    spread: 70,
+    origin: { y: 0.6 },
+  });
+  let duration = 1000;
+  let end = Date.now() + duration;
+
+  (function frame() {
+    confetti({
+      particleCount: 50,
+      spread: 60,
+      origin: { x: Math.random(), y: Math.random() * 0.5 },
+    });
+    if (Date.now() < end) {
+      requestAnimationFrame(frame);
+    }
+  })();
+
+};
+
+const checkWinner=()=>{
+    for(let pattern of winPatterns){//pattetrn is index
+
+        let pos1val=boxes[pattern[0]].innerText;
+        let pos2val=boxes[pattern[1]].innerText;
+        let pos3val=boxes[pattern[2]].innerText;
+        if (pos1val!=""&& pos2val!=""&& pos3val!=""){
+            if(pos1val===pos2val&& pos2val==pos3val){
+                 pattern.forEach((index) => {
+                boxes[index].classList.add("winning-box");
+      });
+                console.log("Winner ",pos1val);
+                showWinner(pos1val);
+                
+            }
+        }
+  
+    };
+
+};
+newGamebtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);
+
